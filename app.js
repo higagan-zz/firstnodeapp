b01dcdc0513fa4250638fb0d67e03b5ff5950133
Gagan Deep <gagan.ping@gmail.com>
2019-04-20 12:05:34 +0530

    add index

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'))
+app.get('/login', (req, res) => res.json({'name':'gagan','add':'blr'}))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
